0e6c5c46719638c30e1afa69443e1993ad1b2e88
Sungho Baek <ashsh0412@gmail.com>
2025-03-08 00:29:05 -0500

    Project 1 finish

--- Project1/project1.h
+++ Project1/project1.h
@@ -1,233 +1,338 @@
 #include <iostream>
 #include <vector>
-#include <stdexcept> // out_of_range 예외를 위해 필요
+#include <stdexcept>
 
-// 템플릿 클래스 LinkedList 정의
 template <typename T>
 class LinkedList {
 public:
-    // Node 구조체: LinkedList의 구성요소
     struct Node {
-        T data;          // 데이터 저장
-        Node* next;      // 다음 노드 가리킴
-        Node* prev;      // 이전 노드 가리킴
-        Node(const T& value) : data(value), next(nullptr), prev(nullptr) {}  // 생성자
+        T data;
+        Node* next;
+        Node* prev;
+
+        Node(const T& value) {
+            data = value;
+            next = nullptr;
+            prev = nullptr;
+        }
     };
 
 private:
-    Node* head;    // 리스트의 첫 노드
-    Node* tail;    // 리스트의 마지막 노드
-    size_t nodeCount; // 현재 노드 개수
+    Node* head;
+    Node* tail;
+    unsigned int nodeCount;
 
 public:
-    // 기본 생성자: 빈 리스트 초기화
-    LinkedList() : head(nullptr), tail(nullptr), nodeCount(0) {}
+    LinkedList() {
+        head = nullptr;
+        tail = nullptr;
+        nodeCount = 0;
+    }
 
-    // 소멸자: 모든 노드 삭제
-    ~LinkedList() { Clear(); }
+    LinkedList(const LinkedList<T>& other) {
+        head = nullptr;
+        tail = nullptr;
+        nodeCount = 0;
 
-    // 복사 생성자: 깊은 복사로 다른 리스트를 복제
-    LinkedList(const LinkedList& other) : head(nullptr), tail(nullptr), nodeCount(0) {
+        if (other.head == nullptr) {
+            return;
+        }
+
         Node* current = other.head;
         while (current != nullptr) {
-            AddTail(current->data);  // 하나씩 추가
+            AddTail(current->data);
             current = current->next;
         }
     }
 
-    // 복사 대입 연산자: 기존 데이터 삭제하고 깊은 복사
-    LinkedList& operator=(const LinkedList& other) {
-        if (this != &other) {
-            Clear();
-            Node* current = other.head;
-            while (current != nullptr) {
-                AddTail(current->data);
-                current = current->next;
-            }
+    ~LinkedList() {
+        while (head != nullptr) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
         }
+        tail = nullptr;
+        nodeCount = 0;
+    }
+
+    LinkedList<T>& operator=(const LinkedList<T>& other) {
+        if (this == &other) {
+            return *this;
+        }
+
+        while (head != nullptr) { // delete original
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+        head = nullptr;
+        tail = nullptr;
+        nodeCount = 0;
+
+        Node* current = other.head;
+        while (current != nullptr) { // copy new
+            AddTail(current->data);
+            current = current->next;
+        }
+
         return *this;
     }
 
-    // 리스트 크기 반환
-    size_t NodeCount() const { return nodeCount; }
+    T& operator[](unsigned int index) {
+        Node* node = GetNode(index);
+        return node->data;
+    }
 
-    // head, tail 포인터 반환 (읽기/쓰기용)
-    Node* GetHead() { return head; }
-    const Node* GetHead() const { return head; }
-    Node* GetTail() { return tail; }
-    const Node* GetTail() const { return tail; }
+    const T& operator[](unsigned int index) const {
+        const Node* node = GetNode(index);
+        return node->data;
+    }
 
-    // AddHead: 맨 앞에 노드 추가
+    unsigned int NodeCount() const {
+        return nodeCount;
+    }
+
+    Node* GetHead() {
+        return head;
+    }
+
+    const Node* GetHead() const {
+        return head;
+    }
+
+    Node* GetTail() {
+        return tail;
+    }
+
+    const Node* GetTail() const {
+        return tail;
+    }
+
+    Node* Find(const T& value) const {
+        Node* current = head;
+        while (current != nullptr) {
+            if (current->data == value) {
+                return current;
+            }
+            current = current->next;
+        }
+        return nullptr;
+    }
+
+    void FindAll(std::vector<Node*>& outData, const T& value) const {
+        Node* current = head;
+        while (current != nullptr) {
+            if (current->data == value) {
+                outData.push_back(current);
+            }
+            current = current->next;
+        }
+    }
+
+    Node* GetNode(unsigned int index) {
+        if (index >= nodeCount) {
+            throw std::out_of_range("Index out of range");
+        }
+
+        Node* current = head;
+        for (unsigned int i = 0; i < index; i++) {
+            current = current->next;
+        }
+        return current;
+    }
+
+    const Node* GetNode(unsigned int index) const {
+        if (index >= nodeCount) {
+            throw std::out_of_range("Index out of range");
+        }
+
+        const Node* current = head;
+        for (unsigned int i = 0; i < index; i++) {
+            current = current->next;
+        }
+        return current;
+    }
+
     void AddHead(const T& value) {
         Node* newNode = new Node(value);
-        if (head == nullptr) head = tail = newNode;
-        else {
-            newNode->next = head;
-            head->prev = newNode;
+
+        if (head == nullptr) {
             head = newNode;
+            tail = newNode;
+        }
+        else {
+            newNode->next = head; // newNode -> head
+            head->prev = newNode; // newNode <- head
+            head = newNode; // https://stackoverflow.com/questions/46575583/how-to-add-node-to-front-of-linked-list-c-im-overthinking-the-concept
         }
         nodeCount++;
     }
 
-    // AddTail: 맨 뒤에 노드 추가
     void AddTail(const T& value) {
         Node* newNode = new Node(value);
-        if (tail == nullptr) head = tail = newNode;
+
+        if (tail == nullptr) {
+            head = newNode;
+            tail = newNode;
+        }
         else {
+            newNode->prev = tail; // opposite to AddHead
             tail->next = newNode;
-            newNode->prev = tail;
             tail = newNode;
         }
         nodeCount++;
     }
 
-    // 리스트 정방향 출력
+    void AddNodesHead(const T* values, unsigned int count) {
+        for (unsigned int i = count; i > 0; i--) {
+            AddHead(values[i - 1]);
+        }
+    }
+
+    void AddNodesTail(const T* values, unsigned int count) {
+        for (unsigned int i = 0; i < count; i++) {
+            AddTail(values[i]);
+        }
+    }
+
     void PrintForward() const {
-        for (Node* current = head; current; current = current->next) {
+        Node* current = head;
+        while (current != nullptr) {
             std::cout << current->data << std::endl;
+            current = current->next;
         }
     }
 
-    // 리스트 역방향 출력
     void PrintReverse() const {
-        for (Node* current = tail; current; current = current->prev) {
+        Node* current = tail;
+        while (current != nullptr) {
             std::cout << current->data << std::endl;
+            current = current->prev;
         }
     }
 
-    // 재귀 정방향 출력
-    void PrintForwardRecursive(Node* node) const {
-        if (!node) return;
-        std::cout << node->data << std::endl;
-        PrintForwardRecursive(node->next);
+    void InsertBefore(Node* node, const T& value) {
+        Node* newNode = new Node(value);
+        newNode->prev = node->prev;
+        newNode->next = node;
+        node->prev->next = newNode;
+        nodeCount++;
     }
 
-    // 재귀 역방향 출력
-    void PrintReverseRecursive(Node* node) const {
-        if (!node) return;
-        std::cout << node->data << std::endl;
-        PrintReverseRecursive(node->prev);
+    void InsertAfter(Node* node, const T& value) {
+        Node* newNode = new Node(value);
+        newNode->next = node->next;
+        newNode->prev = node;
+        node->next->prev = newNode;
+        node->next = newNode;
+        nodeCount++;
     }
 
-    // 첫 번째 일치 노드 찾기
-    Node* Find(const T& value) {
-        for (Node* current = head; current; current = current->next) {
-            if (current->data == value) return current;
-        }
-        return nullptr;
-    }
-
-    // 모든 일치 노드 찾기
-    void FindAll(std::vector<Node*>& outData, const T& value) const {
-        for (Node* current = head; current; current = current->next) {
-            if (current->data == value) outData.push_back(current);
-        }
-    }
-
-    // 인덱스로 노드 접근
-    Node* GetNode(size_t index) {
-        if (index >= nodeCount) throw std::out_of_range("Index out of range");
-        Node* current = head;
-        for (size_t i = 0; i < index; ++i) current = current->next;
-        return current;
-    }
-
-    const Node* GetNode(size_t index) const {
-        if (index >= nodeCount) throw std::out_of_range("Index out of range");
-        Node* current = head;
-        for (size_t i = 0; i < index; ++i) current = current->next;
-        return current;
-    }
-
-    // 인덱스로 데이터 접근 (배열처럼 사용 가능)
-    T& operator[](size_t index) {
-        return GetNode(index)->data;
-    }
-
-    const T& operator[](size_t index) const {
-        return GetNode(index)->data;
-    }
-
-    // 지정 노드 앞에 삽입
-    bool InsertBefore(Node* node, const T& value) {
-        if (!node) return false;
-        if (node == head) AddHead(value);
-        else {
-            Node* newNode = new Node(value);
-            newNode->prev = node->prev;
-            newNode->next = node;
-            node->prev->next = newNode;
-            node->prev = newNode;
-            nodeCount++;
-        }
-        return true;
-    }
-
-    // 지정 노드 뒤에 삽입
-    bool InsertAfter(Node* node, const T& value) {
-        if (!node) return false;
-        if (node == tail) AddTail(value);
-        else {
-            Node* newNode = new Node(value);
-            newNode->next = node->next;
-            newNode->prev = node;
-            node->next->prev = newNode;
-            node->next = newNode;
-            nodeCount++;
-        }
-        return true;
-    }
-
-    // 인덱스에 삽입
-    void InsertAt(const T& value, size_t index) {
+    void InsertAt(const T& value, unsigned int index) {
         if (index > nodeCount) throw std::out_of_range("Index out of range");
         if (index == 0) AddHead(value);
         else if (index == nodeCount) AddTail(value);
         else InsertBefore(GetNode(index), value);
     }
 
-    // 리스트 비교 연산자
-    bool operator==(const LinkedList& rhs) const {
-        if (nodeCount != rhs.nodeCount) return false;
-        Node* left = head;
-        Node* right = rhs.head;
-        while (left && right) {
-            if (left->data != right->data) return false;
-            left = left->next;
-            right = right->next;
+    bool operator==(const LinkedList& otherList) const {
+        if (nodeCount != otherList.nodeCount) return false;
+
+        Node* currentThis = head;
+        Node* currentOther = otherList.head;
+        while (currentThis && currentOther) {
+            if (currentThis->data != currentOther->data) return false;
+            currentThis = currentThis->next;
+            currentOther = currentOther->next;
         }
+        return currentThis == nullptr && currentOther == nullptr;
+    }
+
+    void PrintForwardRecursive(Node* node) const {
+        if (!node) return;
+        std::cout << node->data << std::endl;
+        PrintForwardRecursive(node->next); // pointing until there is no node to point
+    }
+
+    void PrintReverseRecursive(Node* node) const {
+        if (!node) return;
+        std::cout << node->data << std::endl;
+        PrintReverseRecursive(node->prev); // pointing until there is no node to point
+    }
+
+    bool RemoveHead() {
+        if (!head) return false; // if list is empty
+        Node* temp = head;
+        if (head == tail) {
+          head = nullptr;
+          tail = nullptr;
+        } else {
+            head = head->next;  // set new head before delete original one
+            head->prev = nullptr;
+        }
+        delete temp;
+        nodeCount--;
         return true;
     }
 
-    // 삭제 관련 함수들
-    bool RemoveHead() { return RemoveAt(0); }
-    bool RemoveTail() { return RemoveAt(nodeCount - 1); }
+    bool RemoveTail() {
+        if (!tail) return false; // if list is empty
+        Node* temp = tail;
+        if (head == tail) {
+            head = nullptr;
+            tail = nullptr;
+        } else {
+            tail = tail->prev; // set new tail before delete original one
+            tail->next = nullptr;
+        }
+        delete temp;
+        nodeCount--;
+        return true;
+    }
 
-    bool RemoveAt(size_t index) {
+    bool RemoveAt(unsigned int index) {
         if (index >= nodeCount) return false;
+
+        if (index == 0) return RemoveHead();
+        if (index == nodeCount - 1) return RemoveTail();
+
         Node* node = GetNode(index);
-        if (node == head) head = node->next;
-        if (node == tail) tail = node->prev;
-        if (node->prev) node->prev->next = node->next;
-        if (node->next) node->next->prev = node->prev;
+        Node* prevNode = node->prev;
+        Node* nextNode = node->next;
+
+        prevNode->next = nextNode;
+        nextNode->prev = prevNode;
+
         delete node;
         nodeCount--;
         return true;
     }
 
-    size_t Remove(const T& value) {
-        size_t removed = 0;
-        for (Node* current = head; current;) {
-            if (current->data == value) {
-                Node* toDelete = current;
-                current = current->next;
-                RemoveNode(toDelete);
-                removed++;
+    int Remove(const T& value) {
+        int count = 0;
+        Node* temp = head;
+        Node* prev = nullptr;
+
+        while (temp && temp->data == value) {
+            head = temp->next;
+            delete temp;
+            temp = head;
+            count++;
+        }
+
+        while (temp) {
+            if (temp->data == value) {
+                prev->next = temp->next;
+                delete temp;
+                temp = prev->next;
+                count++;
             } else {
-                current = current->next;
+                prev = temp;
+                temp = temp->next;
             }
         }
-        return removed;
+
+        return count;
     }
 
     void Clear() {
